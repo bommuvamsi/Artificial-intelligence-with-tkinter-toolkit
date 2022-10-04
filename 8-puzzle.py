09a11011be2b00c330259b2a5090f73fc9991242
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 17:04:49 +0530

    Update 8-puzzle.py

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -145,3 +145,28 @@ final = [ [ 1, 2, 3 ],
 
 empty_tile_posi = [ 1, 2 ]  
 solve(initial, empty_tile_posi, final)  
+
+
+output :- 
+ 
+ Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================ RESTART: V:\8-puzzle.py ===========================
+1  2  3  
+5  6  0  
+7  8  4  
+
+1  2  3  
+5  0  6  
+7  8  4  
+
+1  2  3  
+5  8  6  
+7  0  4  
+
+1  2  3  
+5  8  6  
+0  7  4  
+
+
