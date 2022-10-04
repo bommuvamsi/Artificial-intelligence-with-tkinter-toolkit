0407ee335f60367bc0fdec987ead44872a51f581
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 16:57:47 +0530

    Update candibal problem.py

--- candibal problem.py
+++ candibal problem.py
@@ -90,3 +90,39 @@ try:
 			break
 except EOFError as e:
 	print("\nInvalid input please retry !!")
+
+output :-
+	
+======================== RESTART: V:\candibal problem.py =======================
+
+
+	Game Start
+Now the task is to move all of them to right side of the river
+rules:
+1. The boat can carry at most two people
+2. If cannibals num greater then missionaries then the cannibals would eat the missionaries
+3. The boat cannot cross the river by itself with no people on board
+
+M M M C C C |	 --- | 
+
+Left side -> right side river travel
+Enter number of Missionaries travel => 2
+Enter number of Cannibals travel => 1
+Wrong input re-enter : 
+Left side -> right side river travel
+Enter number of Missionaries travel => 1
+Enter number of Cannibals travel => 2
+Wrong input re-enter : 
+Left side -> right side river travel
+Enter number of Missionaries travel => 1
+Enter number of Cannibals travel => 1
+
+
+M M C C | --> | M C 
+
+Right side -> Left side river travel
+Enter number of Missionaries travel => 1
+Enter number of Cannibals travel => 1
+
+
+M M M C C C | <-- | 
