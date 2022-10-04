afbdef2a65016ccdf7a0cde8e4c76595dd469e8e
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 14:39:30 +0530

    Update vaccum cleaner.py

--- vaccum cleaner.py
+++ vaccum cleaner.py
@@ -103,3 +103,25 @@ def vacuum_world():
 
 
 vacuum_world()
+
+output :-
+        
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================ RESTART: V:\8-puzzle.py ===========================
+1  2  3  
+5  6  0  
+7  8  4  
+
+1  2  3  
+5  0  6  
+7  8  4  
+
+1  2  3  
+5  8  6  
+7  0  4  
+
+1  2  3  
+5  8  6  
+0  7  4  
