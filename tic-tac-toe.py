1f267d20bc846c583f9a0833f8f263f46772c28e
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 14:49:32 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -102,3 +102,46 @@ def play_game():
 
 # Driver Code
 print("Winner is: " + str(play_game()))
+
+
+output :-
+[[0 0 0]
+ [0 0 0]
+ [0 0 0]]
+Board after 1 move
+[[0 0 0]
+ [0 0 1]
+ [0 0 0]]
+Board after 2 move
+[[0 0 0]
+ [2 0 1]
+ [0 0 0]]
+Board after 3 move
+[[0 0 0]
+ [2 1 1]
+ [0 0 0]]
+Board after 4 move
+[[0 2 0]
+ [2 1 1]
+ [0 0 0]]
+Board after 5 move
+[[0 2 1]
+ [2 1 1]
+ [0 0 0]]
+Board after 6 move
+[[0 2 1]
+ [2 1 1]
+ [0 0 2]]
+Board after 7 move
+[[1 2 1]
+ [2 1 1]
+ [0 0 2]]
+Board after 8 move
+[[1 2 1]
+ [2 1 1]
+ [0 2 2]]
+Board after 9 move
+[[1 2 1]
+ [2 1 1]
+ [1 2 2]]
+Winner is: 1
