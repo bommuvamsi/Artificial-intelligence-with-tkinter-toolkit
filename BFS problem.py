181a0c6a8a62c31037e7c0ecc73febd60183f317
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 14:33:31 +0530

    Update BFS problem.py

--- BFS problem.py
+++ BFS problem.py
@@ -45,3 +45,12 @@ print ("Following is Breadth First Traversal"
 g.BFS(2)
 
 
+output :-
+	
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================== RESTART: V:\BFS problem.py ==========================
+Following is Breadth First Traversal (starting from vertex 2)
+2 0 3 1 
+
