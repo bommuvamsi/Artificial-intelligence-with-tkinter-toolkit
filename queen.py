37d9806482ec5207a6caff05a2a7a9cd3df058e8
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-04 14:51:17 +0530

    Update queen.py

--- queen.py
+++ queen.py
@@ -27,3 +27,16 @@ def N_queens(n):
 N_queens(N)
 for i in board:
     print (i)
+
+    output :-
+     
+ Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================= RESTART: V:\queen.py =============================
+Enter the number of queens
+4
+[0, 1, 0, 0]
+[0, 0, 0, 1]
+[1, 0, 0, 0]
+[0, 0, 1, 0]
