810af6fb8077c4da14f5953a5864cc413fc83a0c
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-09 23:02:08 +0530

    Update matrix transpose.py

--- matrix transpose.py
+++ matrix transpose.py
@@ -13,3 +13,13 @@ for i in range(len(X)):
 
 for r in result:
    print(r)
+
+
+output :-
+    
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================== RESTART: V:/matrix transpose.py =======================
+[12, 4, 3]
+[7, 5, 8]
