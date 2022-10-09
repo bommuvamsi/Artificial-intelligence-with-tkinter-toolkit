133fa2c734202cc474eba89e843bc7ed46eadead
VAMSI KRISHNA <113424074+bommuvamsi@users.noreply.github.com>
2022-10-09 23:20:31 +0530

    Update set operations.py

--- set operations.py
+++ set operations.py
@@ -12,3 +12,12 @@ print("Difference of E and N is",E - N)
 
 
 print("Symmetric difference of E and N is",E ^ N)
+
+
+output :-
+  
+========================= RESTART: V:/set operations.py ========================
+Union of E and N is {0, 1, 2, 3, 4, 5, 6, 8}
+Intersection of E and N is {2, 4}
+Difference of E and N is {0, 8, 6}
+Symmetric difference of E and N is {0, 1, 3, 5, 6, 8}
